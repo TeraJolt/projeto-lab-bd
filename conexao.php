aa3ab1eaef1262f66c3bd69b7525a3efbc09cab7
TeraJolt <jollimas14@outlook.com>
2024-05-14 19:09:00 -0300

    criada conexão

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+    $servidor='localhost';
+    $usuario='root';
+    $senha= '';
+    $db='db_loja';
+    $con=mysqli_connect($servidor,$usuario,$senha,$db);
+    if (!$con){
+        print("Erro na conexão com MySQL");
+        print("Erro: " . mysqli_connect_error());
+        exit;
+    }
+    
+?>
