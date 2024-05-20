4eb46911ae3e830381515f853b790204d7cb118e
Guilherme Melo de Lima <limaguilherme.melo@gmail.com>
2024-05-20 20:17:30 -0300

    update consultar cliente e consultar produto

--- cliente/consultarCliente.php
+++ cliente/consultarCliente.php
@@ -8,29 +8,55 @@
 <body>
     <h1>Consultar Clientes</h1>
     <?php include('../navbar.php') ?>
-    <form> 
-        <table>
-            <tr>
-            <td class='id'>Id</td>    
-            <td class='titulo' colspan='3'>Nome</td>
-            </tr>
-            <?php
-                include('../conexao.php');
+    <form>
+        <label>Filtrar por Nome:</label>
+        <input type="text" id="filtro_nome" name="filtro_nome"><br>
+        <label>Filtrar por Cidade:</label>
+        <input type="text" id="filtro_cidade" name="filtro_cidade"><br>
+        <button type="submit">Filtrar</button>
+    </form> 
+    <table>
+        <?php
+            include('../conexao.php');
 
-                $query="SELECT * FROM tb_cliente ORDER BY id";
-                $resu = mysqli_query($con,$query) or die(mysqli_connect_error());
+            $query = "SELECT * FROM tb_cliente";
 
-                while ($reg = mysqli_fetch_array($resu)) {
-                    echo "<tr class='linha'><td class='id'>".$reg['id']."</td>";
-                    echo "<td class='nome'>".$reg['nome']."</td>";                        
-                    echo "<td class='editar'><a href='atualizarCliente.php?id=".$reg['id']."' class='txtEditEditar'>Editar</a></td>";
-                    echo "<td class='excluir'><a href='excluirCliente.php?id=".$reg['id']."' class='txtEditExcluir'>Excluir 🗑</a></td></tr>";
-                }
-            ?>
-        </table>    
-    </form>
-    <?php
-        mysqli_close($con);
-    ?>  
+            if(isset($_GET['filtro_nome']) && isset($_GET['filtro_cidade'])){
+                $filtro_nome = $_GET['filtro_nome'];
+                $filtro_cidade = $_GET['filtro_cidade'];
+                $query .= " WHERE nome LIKE '%$filtro_nome%' AND cidade LIKE '%$filtro_cidade%'";
+
+            } elseif(isset($_GET['filtro_nome'])){
+                $filtro_nome = $_GET['filtro_nome'];
+                $query .= " WHERE nome LIKE '%$filtro_nome%'";
+
+            } elseif(isset($_GET['filtro_cidade'])){
+                $filtro_cidade = $_GET['filtro_cidade'];
+                $query .= " WHERE cidade LIKE '%$filtro_cidade%'";
+            }
+
+            $query .= " ORDER BY id";
+            $resu = mysqli_query($con,$query) or die(mysqli_connect_error());
+
+            echo "<tr>";
+            echo "<td>Id</td>    ";
+            echo "<td> <b> Nome </b></td>";
+            echo "<td> <b> Cidade </b></td>";
+            echo "<td> <b> email </b></td>";
+            echo "<td> <b> CPF/CNPJ </b></td>";
+            echo "</tr>";
+
+            while ($reg = mysqli_fetch_array($resu)) {
+                echo "<tr class='linha'><td class='id'>".$reg['id']."</td>";
+                echo "<td class='nome'>".$reg['nome']."</td>";                        
+                echo "<td class='cidade'>".$reg['cidade']."</td>";
+                echo "<td class='email'>".$reg['email']."</td>";
+                echo "<td class='cpf_cnpj'>".$reg['cpf_cnpj']."</td>";
+                echo "<td class='editar'><a href='atualizarCliente.php?id=".$reg['id']."' class='txtEditEditar'>Editar</a></td>";
+                echo "<td class='excluir'><a href='excluirCliente.php?id=".$reg['id']."' class='txtEditExcluir'>Excluir 🗑</a></td></tr>";
+            }
+            mysqli_close($con);
+        ?>
+    </table>    
 </body>
-</html
+</html>
